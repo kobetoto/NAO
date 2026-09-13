3f0ae44bff1eb811296f3d411bd1e57ed5c9cff8
kobetoto <thodavid@student.42.fr>
2026-09-13 14:48:01 +0200

    init + first step [-say: intro]

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Intro</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bonjour ! Je suis NAO. Aujourd'hui on va réveiller le corps avant de réveiller la tête.
+Vous faites comme moi, mais à votre rythme! Le yoga, ce n'est pas une compétition. C'est une histoire!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Bonjour ! Je suis NAO. Aujourd'hui on va réveiller le corps avant de réveiller la tête.
+Vous faites comme moi, mais à votre rythme! Le yoga, ce n'est pas une compétition. C'est une histoire!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>
