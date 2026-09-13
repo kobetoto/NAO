cc5e723dbfac6080803ad485939d2dcd31456fa1
kobetoto <thodavid@student.42.fr>
2026-09-13 16:38:52 +0200

    king position: OK

--- translations/translation_en_US.ts
+++ translations/translation_en_US.ts
@@ -2,6 +2,34 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
     <context>
+        <name>behavior_1/behavior.xar:/Bravo</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bravo! Petits Humains!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Bravo! Petits Humains!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Err</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>OH OH</source>
+            <comment>Text</comment>
+            <translation type="unfinished">OH OH</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Instructions QUEEN/KING</name>
+        <message>
+            <source>En position du tailleur, les mains sur les genoux, tête haute et dos droit.
+Moi je m'assois comme je peux, avec mon corps de robot    vous, vous pouvez croiser les jambes!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">En position du tailleur, les mains sur les genoux, tête haute et dos droit.
+Moi je m'assois comme je peux, avec mon corps de robot    vous, vous pouvez croiser les jambes!</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Intro</name>
         <message>
             <source>Hello</source>
@@ -9,12 +37,37 @@
             <translation type="obsolete">Hello</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Bonjour ! Je suis NAO. Aujourd'hui on va réveiller le corps avant de réveiller la tête.
 Vous faites comme moi, mais à votre rythme! Le yoga, ce n'est pas une compétition. C'est une histoire!</source>
             <comment>Text</comment>
-            <translation type="unfinished">Bonjour ! Je suis NAO. Aujourd'hui on va réveiller le corps avant de réveiller la tête.
+            <translation type="obsolete">Bonjour ! Je suis NAO. Aujourd'hui on va réveiller le corps avant de réveiller la tête.
 Vous faites comme moi, mais à votre rythme! Le yoga, ce n'est pas une compétition. C'est une histoire!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bonjour ! Je suis NAO. Aujourd'hui on va réveiller le corps avant de réveiller la tête. Vous faites comme moi, mais à votre rythme! Le yoga, ce n'est pas une compétition. C'est une histoire!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Bonjour ! Je suis NAO. Aujourd'hui on va réveiller le corps avant de réveiller la tête. Vous faites comme moi, mais à votre rythme! Le yoga, ce n'est pas une compétition. C'est une histoire!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/QUEEN/KING 1/2</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>En position du tailleur, les mains sur les genoux, tête haute et dos droit, vous etes des reines et des rois.
+Moi je m'assois comme je peux, avec mon corps de robot! vous, vous pouvez croiser les jambes!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">En position du tailleur, les mains sur les genoux, tête haute et dos droit, vous etes des reines et des rois.
+Moi je m'assois comme je peux, avec mon corps de robot! vous, vous pouvez croiser les jambes!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/QUEEN/KING 2/2</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Doucement, la reine ou le roi regarde son royaume à droite et à gauche</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Doucement, la reine ou le roi regarde son royaume à droite et à gauche</translation>
         </message>
     </context>
     <context>
